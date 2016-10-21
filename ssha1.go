be950b6a363745ec5851ba9ff351046a259cecc7
Cyril Oblikov <munknex@gmail.com>
2016-10-21 13:33:13 +0300

    initial

---
+++ ssha1.go
@@ -0,0 +1,15 @@
+package ssha1
+
+import (
+	"github.com/jsimonetti/pwscheme/ssha"
+)
+
+func GenerateFromPassword(pwd []byte, cost uint8) ([]byte, error) {
+	hash, err := ssha.Generate(string(pwd), cost)
+	return []byte(hash), err
+}
+
+func CompareHashAndPassword(hash, pwd []byte) error {
+	_, err := ssha.Validate(string(pwd), string(hash))
+	return err
+}
